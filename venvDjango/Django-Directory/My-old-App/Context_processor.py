a73a06b72171026e99ee572d2753326ea74a22d8
epiczed00 <malcom2000@live.se>
2019-12-31 15:58:43 +0100

    x
    x

--- venvDjango/Django-Directory/My-old-App/Context_processor.py
+++
@@ -1,9 +0,0 @@
-from .models import Pictures
-
-
-def Context_Pictures(request):
-
-    Picture_context = Pictures.objects.get(pk=1)
-    context = {"Picture_context":Picture_context}
-
-    return context
